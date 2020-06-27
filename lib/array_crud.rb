f910650cd05d164c0d60f0ef1565b4e92b69328a
Andrea Tchalaou <attchalaou@gmail.com>
2020-06-27 14:11:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,9 +13,7 @@ def add_element_to_end_of_array(array, element)
  end
 
 def add_element_to_start_of_array(array, element)
-  # array = ["I", "am", "really", "learning"]
-  # element = "wow"
-  # puts array.unshift (element)
+  create_an_array.first element
     
 end
 
